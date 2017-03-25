b0b51054f567c3ba7651456bf92c1142af4546cb
quan <527832822@qq.com>
2017-03-25 18:33:36 +0800

    add validation to title

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -25,6 +25,16 @@ class GroupsController < ApplicationController
              redirect_to groups_path
         end
 
+        def create
+   @group = Group.new(group_params)
+
+   if @group.save
+    redirect_to groups_path
+  else
+    render :new
+  end
+end
+
 
 
 
