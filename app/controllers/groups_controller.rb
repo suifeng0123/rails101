d1774ed0905b35a07f706a41b30a4752e80401df
quan <527832822@qq.com>
2017-03-26 13:45:15 +0800

    use simple_format helper to decorate

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,11 +1,10 @@
 class GroupsController < ApplicationController
     before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
     before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
-    before_action :authenticate_user!
-    def index
-        @groups = current_user.participated_groups
-      end
-    
+  
+  def index
+    @groups = Group.all
+  end
   def show
     @group = Group.find(params[:id])
     @posts = @group.posts.recent.paginate(:page => params[:page], :per_page => 5)
