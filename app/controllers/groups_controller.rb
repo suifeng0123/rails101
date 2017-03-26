64125096498db1792806d735ef79234011b9dcf2
quan <527832822@qq.com>
2017-03-26 13:35:06 +0800

    implement my groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,11 @@
 class GroupsController < ApplicationController
     before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
-     before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
-  def index
-    @groups = Group.all
-  end
+    before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
+    before_action :authenticate_user!
+    def index
+        @groups = current_user.participated_groups
+      end
+    
   def show
     @group = Group.find(params[:id])
     @posts = @group.posts.recent.paginate(:page => params[:page], :per_page => 5)
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :groups do
-
-
    member do
    post :join
    post :quit
@@ -11,6 +9,10 @@ Rails.application.routes.draw do
 
   resources :posts
   end
-  root 'groups#index'
+
+   namespace :account do
+   resources :groups
+   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+     root 'groups#index' 
 end
